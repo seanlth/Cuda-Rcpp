12cd504497c793c25ba13f7bd24f6936f7b87933
Caleb Prior <cprior@tcd.ie>
2015-03-08 15:01:24 +0000

    Commented code
    Added comments to rcuda.cpp and test.R to explain their operations

--- rcuda.cpp
+++ rcuda.cpp
@@ -1,40 +1,57 @@
 #include <Rcpp.h>
 #include<stdio.h>
-
 #include "Kernels.h"
 using namespace Rcpp;
 
+// Function to convert a Rcpp:List of vectors to a 2d double array
 double** listTo2dDouble(Rcpp::List list)
 {
+  // Get length of list and allocate size of result array
   int length = list.size();
   double ** temp = new double*[length];
   
   for(int i=0; i<length; i++) {
+    // Retrieve the vector at position i from the list
     std::vector<double> test = as<std::vector<double> >(list[i]);
-    int size = test.size();
     
+	// Allocate space for the result array
+	int size = test.size();
     temp[i] = new double[size];
+	
+	// Copy elements from vector to a double array
     for(int j=0; j<size; j++)
     {
       temp[i][j] = test[j];
     }
   }
+  
+  // Return the 2d double array 
   return temp;
 }
 
+// This is the exported Rcpp function that can be called from R
 RcppExport SEXP cudaLog(SEXP a1, SEXP b1, SEXP t1, SEXP numIter)
 {
+  // Convert parameters from SEXP objects
   int iterations = Rcpp::as<int>(numIter);
   Rcpp::List a(a1);
   Rcpp::List b(b1);
+  Rcpp::NumericVector data(t1);
+  
+  // Length of a vector from a or b (Should be the same)
   int len = as<std::vector<double> >(a[0]).size();
   
+  // Convert a and b from a list of vectors to a 2d double array
   double ** cA = listTo2dDouble(a);
   double ** cB = listTo2dDouble(b);
-  Rcpp::NumericVector data(t1);
+
+  // Call the CUDA implementation of the log-likelihood function from Kernels.h with signature:
+  // double* CUDALogLikelihood(double* data, unsigned int data_length, double** a, double** b, unsigned int vector_length, unsigned int iterations)
   double* res = CUDALogLikelihood(data.begin(), data.size(), cA, cB, len, iterations);
   
+  // Convert the resultant double array to a double vector
   std::vector<double> result(res, res + iterations);
   
+  // Wrap the resultant vector to a R object and return it
   return Rcpp::wrap(result);
 }
--- test.R
+++ test.R
@@ -1,11 +1,17 @@
+# Load the shared library containing the log-likelihood
+# function that runs through CUDA
 library(Rcpp)
 dyn.load("rcuda.so")
 
+# Wrapper function to call the CUDA implementation of
+# the log-likelihood function
 cudaLog <- function(a, b, t, iterations) 
 {
   .Call("cudaLog", a, b, t, iterations)
 }
 
+# This is the function in R that is implemented in the above CUDA
+# function, it is used to verify that the results are the same
 fn1 <- function(a, b, t) {
   N <- length(t)
   nv <- length(a)
@@ -19,26 +25,31 @@ fn1 <- function(a, b, t) {
   sum(TMP)
 }
 
-ITER <- 10000
-NVER <- 100 # fake version vector length
+ITER <- 10000	# The number of iterations to run
+NVER <- 100 	# fake version vector length
 
 cat('Number of Iterations: ', ITER, '\n')
 cat('Vector size: ', NVER, '\n')
 
+# Setting up a and b
 a <- vector(mode='list', length=ITER)
 b <- vector(mode='list', length=ITER)
 
-set.seed(123) # to make sure the fake data is the same each time!
-data <- runif(10) # fake data
-RES <- vector(mode='numeric', length=ITER)
+set.seed(123) 		# to make sure the fake data is the same each time!
+data <- runif(10) 	# fake data
+# Used to hold the results from the R implementation
+RES <- vector(mode='numeric', length=ITER)	 
+# Used to hold the results from the CUDA implementation
 RES1 <- vector(mode='numeric', length=ITER) 
 
+# Filling a and b with fake data
 for(i in 1:ITER)
 {
   a[[i]] <- runif(NVER)
   b[[i]] <- runif(NVER)
 }
 
+# Running the R version of the function and timing it
 T0 <- Sys.time()
 for (i in 1:ITER) {
   RES[[i]] <- fn1(a[[i]], b[[i]], data)
@@ -48,6 +59,7 @@ TD <- T1-T0
 
 cat('Time taken R: ', TD, units(TD), '\n')
 
+# Running the CUDA version of the function and timing it
 T2 <- Sys.time()
 RES1 <- cudaLog(a, b, data, ITER)
 T3 <- Sys.time()
@@ -55,4 +67,5 @@ TD1 <- T3-T2
 
 cat('Time taken CUDA: ', TD1, units(TD1), '\n')
 
+# Checking that the results are the same
 cat("All Equal? ",all.equal(RES, RES1), '\n')
