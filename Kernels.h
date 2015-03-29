1da53f0ae104b6fd4cd7f2e4f629c733a57dd243
Caleb Prior <cprior@tcd.ie>
2015-03-29 20:13:47 +0100

    Cleanup of Repository
    Removed unused functions and files

--- Kernels.h
+++ Kernels.h
@@ -4,34 +4,6 @@
 #include <iostream>
 #include <stdio.h>
 
-class Matrix {
-public:
-    float* values;
-    int columns;
-    int rows;
-    
-    Matrix() {}
-    
-    Matrix(int columns, int rows) {
-        this->columns = columns;
-        this->rows = rows;
-    }
-};
-
-
-//matrix operations
-Matrix CUDAMatrixMul(Matrix h_A, Matrix h_B);
-
-//vector operations
-int* SERIALVectorAdd(int* a, int* b, int length);
-int* CUDAVectorAdd(int* a, int* b, int length);
-int* CUDAVectorSub(int* a, int* b, int length);
-int* CUDAVectorDiv(int* a, int* b, int length);
-int* CUDAVectorMul(int* a, int* b, int length);
-
-double* SERIALLogLikelihood(double* data, unsigned int data_length, double** a, double** b, unsigned int vector_length, unsigned int iterations);
 double* CUDALogLikelihood(double* data, unsigned int data_length, double** a, double** b, unsigned int vector_length, unsigned int iterations);
-
-
 
 #endif
--- Testing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for (( i=10; i<=1000; i+=20 ))
-do
-    ./R_CUDA $i
-done
-
